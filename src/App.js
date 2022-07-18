0ce0394d0b32c8ce5dba43855eed1e67326ffc15
Wiebke Bienk <wiebke.bienk@gmail.com>
2022-07-18 11:55:45 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+const { isbn10Checksum, isbn13Checksum } = require('isbn-check/src/isbn-check');
+let bookISBN = require('node-isbn');
 
 function App() {
 
-  const {isbn10Checksum, isbn13Checksum} = require('isbn-check/src/isbn-check');
   const [isbnNum, ISBNString] = useState('');
+  let [result] = useState('');
+  const [bookInfo] = useState(null);
 
-  let [resultMessage, setResultMessage] = useState('');
-  let result = false;
-  
+  let authors = [];
+  let thumbnailLink = '';
 
-  function Check(butoon){
-    ISBNString(butoon.target.value)
+  function Check(button) {
+    ISBNString(button.target.value)
   }
 
-  function checkISBN(isbnNum){
-    if (isbnNum.length === 10) { 
+  function checkISBN(isbnNum) {
+    if (isbnNum.length === 10) {
       result = isbn10Checksum(isbnNum.slice(0, -1));
     }
-    if (isbnNum.length === 13) { 
+    if (isbnNum.length === 13) {
       result = isbn13Checksum(isbnNum.slice(0, -1));
     }
     console.log(result);
 
-    if (result == isbnNum.slice(-1)){
-      result('ISBN valid');
-    }else{
+    if (result == isbnNum.slice(-1)) {
+      result('');
+      isbn.provider(['openlibrary']).resolve(isbnString, function (err, book) {
+        if (err) {
+          result('ISBN is valid but book was not found');
+          setBookInfo();
+        } else {
+          setBookInfo();
+        }
+      });
+    } 
+    else {
       result('ISBN invalid');
+      setBookInfo();
     }
-  }
+    }
+    if (bookInfo) {
+      authors = bookInfo.authors.map((author, index) =>
+        <li key={index}>{author}</li>
+      );
+      thumbnailLink = bookInfo.imageLinks.thumbnail;
+    }
+
 
   return (
     <div className="App">
@@ -38,7 +57,13 @@ function App() {
         <h1>ISBN-Checker</h1>
         <input type="text" value={isbnNum} onInput={Check}></input>
         <button onClick={() => checkISBN(isbnNum)}>Check ISBN</button>
-        <p style={{color: SpeechRecognitionResultList === 'ISBN valid' ? "green" : "red" }}>{result}</p>
+        <p style={{color: result === 'ISBN valid' ? "green" : "red" }}>{result}</p>
+        <div>
+          <h3>{bookInfo ? bookInfo.title : ''}</h3>
+          <ul>{authors}</ul>
+          <p>{bookInfo ? bookInfo.description : ''}</p>
+          <img src={thumbnailLink} />
+        </div>
       </header>
     </div>
   );
